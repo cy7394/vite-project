1f8f37018855857c3654b2f4ef8cc2ae32895ccf
曹阳 <2685709100@qq.com>
2023-03-03 11:24:12 +0800

    feat: 配置prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  $schema: "https://json.schemastore.org/prettierrc",
+  semi: false, //semicolons分号 结尾不允许有分号
+  tabWidth: 2, //每个缩进级别的空格数为2
+  singleQuote: false, //不使用单引号
+  printWidth: 100, //打印宽度，打印机将换行的行宽度，默认为80
+  trailingComma: "es5", //尾随逗号 多行对象或者数据允许有尾随逗号
+  arrowParens: "avoid", //箭头函数 单个参数不允许有括号
+}
